199b87675d0fdba51ebe01d4d68ac6fe04e07a0f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-02 20:00:36 +0100

    Extract FrontPage and Login from Application

--- client/src/application.jsx
+++ client/src/application.jsx
@@ -1,9 +1,27 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, Link, Route, Routes} from "react-router-dom";
+import {useState} from "react";
+
+function FrontPage({accessToken}) {
+    return <>
+        <h1>Hello Github</h1>
+        {accessToken && <div><Link to={"/projects"}>My projects</Link></div>}
+        {accessToken && <div><Link to={"/login/endsession"}>Logout</Link></div>}
+        {!accessToken && <div><Link to={"/login"}>Log in</Link></div>}
+        </>;
+}
+
+function Login({setAccessToken}) {
+    return <h1>Login</h1>;
+}
 
 export function Application() {
+    const [accessToken, setAccessToken] = useState();
+
     return <BrowserRouter>
         <Routes>
-            <Route path={"/"} element={<h1>Hello Github</h1>} />
+            <Route path={"/"} element={<FrontPage accessToken={accessToken} />} />
+            {accessToken && <Route path={"/projects"} element={<h1>My projects</h1>} />}
+            <Route path={"/login/*"} element={<Login setAccessToken={setAccessToken} />} />
             <Route path={"*"} element={<h1>Not found</h1>} />
         </Routes>
     </BrowserRouter>;
