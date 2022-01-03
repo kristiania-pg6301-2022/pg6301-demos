ddfd9b42340eee04b80cd015afe85b1d276e3b56
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-03 09:38:32 +0100

    server.js serves React app

--- server/server.js
+++ server/server.js
@@ -1,7 +1,18 @@
-
 const express = require("express");
+const path = require("path");
 
 const app = express();
+
+
+app.use(express.static(path.join(__dirname, "..", "client", "dist")));
+app.use((req, res, next) => {
+    if (req.method === "GET" && !req.path.startsWith("/api")) {
+        res.sendFile(path.join(__dirname, "..", "client", "dist", "index.html"));
+    } else {
+        next();
+    }
+})
+
 
 const server = app.listen(3000, () => {
     console.log("Started on http://localhost:" + server.address().port)
