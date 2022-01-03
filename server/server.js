3a9f905de9937f0e9776efc7d94d74c4a263895f
Johannes Brodwall <johannes.brodwall@soprasteria.com>
2022-01-03 21:50:11 +0100

    Heroku: Read PORT from environment

--- server/server.js
+++ server/server.js
@@ -19,6 +19,6 @@ app.use((req, res, next) => {
 });
 
 
-const server = app.listen(3000, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log("Listening on http://localhost:" + server.address().port)
 });
